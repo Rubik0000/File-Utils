0aa8f3529e67334d03472420aa3a024176a8952c
oleg <oleg@integral>
2018-10-13 11:23:10 +0300

    initial commit

---
+++ src/fileutils/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fileutils;
+
+/**
+ * @author Oleg
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+  	// TODO Auto-generated method stub
+    System.out.println("test jar");
+  }
+
+}
